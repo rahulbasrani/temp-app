c4b19ebbbfc2790fe07d360037b38e85373ee2d5
ravi-basrani <rahulbasranikhairthal@gmail.com>
2021-05-06 05:29:46 +0530

    commit

--- src/app/components/signup/signup.component.tsx
+++ src/app/components/signup/signup.component.tsx
@@ -118,8 +118,7 @@ const SignupForm: React.FC = () => {
           setDisable(false);
           hideLoader();
           setComponentState(ComponentViewState.ERROR);
-          console.log(response.error);
-          if (response.error == "Error: Request failed with status code 404") {
+          if (response.error == "Error: 404") {
             toast(
               <div className="toast-class">
                 <span>
@@ -138,7 +137,7 @@ const SignupForm: React.FC = () => {
               }
             );
           }
-          if (response.error == "Error: Request failed with status code 400") {
+          if (response.error == "Error: 400") {
             toast(
               <div className="toast-class">
                 <span>
--- src/app/services/registration/signup.service.implement.tsx
+++ src/app/services/registration/signup.service.implement.tsx
@@ -2,7 +2,6 @@ import { ServiceResponse } from "../api";
 import { SignupService } from "./signup.service";
 import { RegistrationForm } from "@models";
 import Config from "@config";
-import axios from "axios";
 const baseUrl = Config.baseUrl;
 
 export default class SignupServiceImplement implements SignupService {
@@ -22,16 +21,18 @@ export default class SignupServiceImplement implements SignupService {
       organizationName: organizationName,
     };
     try {
-      const res = await axios.post(`${baseUrl}`, data, {
+      const res = await fetch(`${baseUrl}`, {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
+        body: JSON.stringify(data),
       });
       if (res.status === 400) {
-        throw new Error();
+        throw new Error("400");
       }
       if (res.status === 404) {
-        throw new Error();
+        throw new Error("404");
       }
       return new ServiceResponse<RegistrationForm>(data);
     } catch (error) {
